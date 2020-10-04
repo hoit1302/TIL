647cc03c64c79b958adbe32292e4881fa742a431
hoit1302 <jueun000101@gmail.com>
2020-10-05 08:34:17 +0900

    배열

---
+++ data-structure/05 스택/array-stack.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define MAX_STACK_SIZE 100
+
+typedef int element;
+typedef struct {
+	element stack[MAX_STACK_SIZE];
+	int top;
+} StackType;
+// 스택 초기화 함수
+void init(StackType* s) {
+	s->top = -1;
+}
+// 공백상태 검출 함수
+int is_empty(StackType* s) {
+	return (s->top == -1);
+}
+// 포화상태 검출 함수
+int is_full(StackType* s) {
+	return (s ->top == (MAX_STACK_SIZE-1));
+}
+// 삽입 함수
+void push(StackType* s, element item) {
+	if (is_full(s)) {
+		fprintf(stderr, "스택 포화 에러\n");
+		return;
+	}
+	else s->stack[++(s->top)] = item;
+}
+// 삭제 함수
+element pop(StackType* s) {
+	if (is_empty(s)) {
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else return s->stack[(s->top)--];
+}
+// 피크 함수
+element peek(StackType* s) {
+	if (is_empty(s)) {
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else return s->stack[s->top];
+}
