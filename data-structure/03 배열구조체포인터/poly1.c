ce791519db3add53c2c5e99b32f70b74a4b170e2
hoit1302 <jueun000101@gmail.com>
2020-10-05 08:33:43 +0900

    다항식1-배열

---
+++ data-structure/03 배열구조체포인터/poly1.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define MAX_DEGREE 101
+#define MAX(a,b) ((a>b)?a:b)
+
+typedef struct {
+	int degree;
+	int coef[MAX_DEGREE];
+} polynomial;
+
+// C = A+B, 여기서 A와 B는 다항식이다.
+polynomial poly_add1(polynomial A, polynomial B) {
+	polynomial C;
+	int Apos = 0, Bpos = 0, Cpos = 0; // 배열 인덱스 변수
+	int degree_a = A.degree;
+	int degree_b = B.degree;
+	C.degree = MAX(A.degree, B.degree); // 결과 다항식 차수
+	while (Apos <= A.degree && Bpos <= B.degree) {
+		if (degree_a > degree_b) { // A항 > B항
+			C.coef[Cpos++] = A.coef[Apos++];
+			degree_a--;
+		}
+		else if (degree_a == degree_b) { // A항 == B항
+			C.coef[Cpos++] = A.coef[Apos++] + B.coef[Bpos++];
+			degree_a--; degree_b--;
+		}
+		else { // B항 > A항
+			C.coef[Cpos++] = B.coef[Bpos++];
+			degree_b--;
+		}
+	}
+	return C;
+}
+
+
+void main() {
+	polynomial a = { 5, {3, 6, 0, 0, 0, 10} };
+	polynomial b = { 4, {7, 0, 5, 0, 1} };
+	polynomial c;
+	c = poly_add1(a, b);
+	return;
+}
