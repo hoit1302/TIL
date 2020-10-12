b1ad0f4d9ccfd3e9a3b6c84879955c635691a677
hoit1302 <jueun000101@gmail.com>
2020-10-12 19:47:41 +0900

    괄호 검사 프로그램 (배열 스택 사용)

--- data-structure/05 스택/parenthesis.c
+++ data-structure/05 스택/parenthesis.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX_STACK_SIZE 100
 
 typedef int element;
@@ -8,39 +9,41 @@ typedef struct {
 	element stack[MAX_STACK_SIZE];
 	int top;
 } StackType;
-// 스택 초기화 함수
-void init(StackType* s){
-	s->top = -1;
+
+// array-stack.c 파일의 내용 끌어쓰기
+extern void init(StackType* s);
+extern int is_empty(StackType* s);
+extern int is_full(StackType* s);
+extern void push(StackType* s, element item);
+extern element pop(StackType* s);
+
+int check_matching(const char* in) {
+	StackType s;
+	init(&s);
+	for (int i = 0; i < (int)strlen(in); i++) {
+		char ch = in[i];
+		switch (ch) {
+		case '(': case '[': case '{':
+			push(&s, ch);
+			break;
+		case ')': case ']': case '}':
+			if (is_empty(&s)) return 0;
+			else {
+				char open_ch = pop(&s);
+				if ((open_ch == '(' && ch != ')') || (open_ch == '[' && ch != ']') || (open_ch == '{' && ch != '}'))
+					return 0;
+			}
+			break;
+		} // switch
+	} // for
+	if (!is_empty(&s)) return 0;
+	return 1;
 }
-// 공백상태 검출 함수
-int is_empty(StackType* s){
-	return (s->top == -1);
-}
-// 포화상태 검출 함수
-int is_full(StackType* s){
-	return (s->top == (MAX_STACK_SIZE-1));
-}
-// 삽입 함수
-void push(StackType* s, element item) {
-	if (is_full(s)) {
-		fprintf(stderr, "스택 포화 에러\n");
-		return;
-	}
-	else s->stack[++(s->top)] = item;
-}
-// 삭제 함수
-element pop(StackType* s) {
-	if (is_empty(s)) {
-		fprintf(stderr, "스택 공백 에러\n");
-		exit(1);
-	}
-	else return s->stack[(s->top)--];
-}
-// 피크 함수
-element peek(StackType* s) {
-	if (is_empty(s)) {
-		fprintf(stderr, "스택 공백 에러\n");
-		exit(1);
-	}
-	else return s->stack[s->top];
+
+
+void main() {
+	if (check_matching("{A[(i+1)]=0;}") == 1)
+		printf("괄호검사성공\n");
+	else
+		printf("괄호검사실패\n");
 }
