e6f36326c9cd1900112e080a45c6667b896d6cdd
hoit1302 <68107000+hoit1302@users.noreply.github.com>
2020-10-23 06:22:34 +0900

    클래스와 객체

--- JAVA/week2/src/sec01/StudentTest.java
+++ JAVA/week2/src/sec01/StudentTest.java
@@ -4,11 +4,11 @@ public class StudentTest {
 
 	public static void main(String[] args) {
 		Student s1 = new Student();
-		s1.setName("¹ÚÁÖÀº");
+		s1.setName("이름");
 		s1.setAge(21);
 		s1.show();
 		
-		Student s2 = new Student("¹ÚÁÖÀº", 21);
+		Student s2 = new Student("이름", 21);
 		s2.show();
 	}
 }
