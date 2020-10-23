74514a9f9c6f21e0ead31fda0686664038e7628e
hoit1302 <68107000+hoit1302@users.noreply.github.com>
2020-10-23 21:55:27 +0900

    상속과 생성자

--- JAVA/week4/src/sec01/StudentTest.java
+++ JAVA/week4/src/sec01/StudentTest.java
@@ -2,7 +2,7 @@ package sec01;
 
 public class StudentTest {
 	public static void main(String[] args) {
-		Student s1 = new Student("�ڹٽ�", "111111-1111111", 1971019);
+		Student s1 = new Student("자바신", "111111-1111111", 1971000);
 		System.out.println("name:" + s1.name);
 		System.out.println("ssn:" + s1.ssn);
 		System.out.println("stuNo:" + s1.stuNo);
