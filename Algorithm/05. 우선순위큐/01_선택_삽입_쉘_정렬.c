47a5cb94cf7da3ad5b65459575f0076c9d562c40
hoit1302 <jueun000101@gmail.com>
2021-04-15 20:28:30 +0900

    21.04.15

---
+++ Algorithm/05. 우선순위큐/01_선택_삽입_쉘_정렬.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+#define MAX_SIZE 15
+#define SWAP(x, y, t) ((t) = (x), (x) = (y), (y) = (t))
+// 정렬 알고리즘
+
+void selection_sort(int list[], int n) {
+    int least, temp;
+
+    for (int i = 0; i < n - 1; i++) {
+        least = i;
+        for (int j = i + 1; j < n; j++) {
+            if (list[j] < list[least])
+                least = j;
+        }
+        SWAP(list[i], list[least], temp);
+    }
+}
+
+
+void inc_insertion_sort(int list[], int first, int last, int gap) {
+    int i, j, key;
+    for (i = first + gap; i <= last; i = i + gap) {
+        key = list[i];
+        for (j = i - gap; j >= first && list[j] > key; j = j - gap)
+            list[j + gap] = list[j];
+        list[j + gap] = key;
+    }
+}
+
+void insertion_sort(int list[], int n) {
+    int i, j, save;
+    for (i = 1; i < n; i++) {
+        save = list[i];
+        for (j = i - 1; j >= 0 && list[j] > save; j--)
+            list[j + 1] = list[j];
+        list[j + 1] = save;
+    }
+}
+
+void shell_sort(int list[], int n) {
+    int i, gap;
+    for (gap = n / 2; gap > 0; gap = gap / 2) {
+        if (gap % 2 == 0)
+            gap++;
+        for (i = 0; i < gap; i++)
+            inc_insertion_sort(list, i, n - 1, gap);
+    }
+}
+
+void main() {
+    int list[MAX_SIZE];
+    srand(time(NULL));
+
+    for (int i = 0; i < MAX_SIZE; i++) {
+        list[i] = rand() % 100;
+        for (int j = 0; j < i; j++)
+            if (list[i] == list[j])
+                i--;
+    }
+
+    for (int i = 0; i < MAX_SIZE; i++)
+        printf("%d ", list[i]);
+    printf("\n");
+    getchar();
+
+    // selection_sort(list, MAX_SIZE);
+    // insertion_sort(list, MAX_SIZE);
+    shell_sort(list, MAX_SIZE);
+    for (int i = 0; i < MAX_SIZE; i++) {
+        printf("%d ", list[i]);
+        Sleep(500);
+    }
+    printf("\n");
+}
