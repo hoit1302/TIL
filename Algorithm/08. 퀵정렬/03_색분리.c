fedd778f8145f69ea4e512d278e7738d0895428e
hoit1302 <jueun000101@gmail.com>
2021-04-23 23:34:05 +0900

    21.04.23

---
+++ Algorithm/08. 퀵정렬/03_색분리.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_SIZE 15
+#define SWAP(x, y, t) ((t) = (x), (x) = (y), (y) = (t))
+
+int partition(char list[], int left, int right) {
+    int low = left - 1, high = right + 1;
+    char temp;
+    
+    do {
+        do
+            low++;
+        while (list[low] == 'B');
+
+        do
+            high--;
+        while (list[high] == 'R');
+
+        if (low < high)
+            SWAP(list[low], list[high], temp);
+
+        printf("\nlow = %d, high = %d\n", low, high);
+        for (int i = 0; i < MAX_SIZE; i++)
+            printf("[%c] ", list[i]);
+        printf("\n");
+    } while (low < high);
+}
+
+int main() {
+    char list[MAX_SIZE];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_SIZE; i++)
+        if (rand() % 2 == 0)
+            list[i] = 'B';
+        else
+            list[i] = 'R';
+
+    for (int i = 0; i < MAX_SIZE; i++)
+        printf("[%c] ", list[i]);
+    printf("\n");
+    getchar();
+
+    partition(list, 0, MAX_SIZE - 1);
+    for (int i = 0; i < MAX_SIZE; i++)
+        printf("[%c] ", list[i]);
+    printf("\n");
+    return 0;
+}
